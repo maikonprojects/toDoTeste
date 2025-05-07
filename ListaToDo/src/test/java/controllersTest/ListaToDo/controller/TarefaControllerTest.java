4b93159945214c1b7cb1069bac653a38d77d936b
MONICA JIULIANI LEAMARI <monica.leamari@solutis.net.br>
2025-05-07 14:42:34 -0300

    Testes de adicionar e deletar tarefa

--- ListaToDo/src/test/java/controllersTest/ListaToDo/controller/TarefaControllerTest.java
+++ ListaToDo/src/test/java/controllersTest/ListaToDo/controller/TarefaControllerTest.java
@@ -7,16 +7,18 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
 import org.springframework.test.context.bean.override.mockito.MockitoBean;
 import org.springframework.test.web.servlet.MockMvc;
 
 import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.doNothing;
 import static org.mockito.Mockito.when;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 @SpringBootTest
 @AutoConfigureMockMvc
@@ -45,10 +47,32 @@ class TarefaControllerTest {
     }
 
     @Test
-    void adicionarTarefa() {
+    void adicionarTarefa() throws Exception{
+
+        Tarefa tarefa = new Tarefa();
+        tarefa.setNome("Escovar");
+        when(servico.cadastrar(any(Tarefa.class))).thenReturn(tarefa);
+
+        String retorno = "Tarefa adicionada: " + tarefa.getNome();
+
+        mockMvc.perform(post("/tarefas")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(objectMapper.writeValueAsString(tarefa)))
+                .andExpect(status().isCreated())
+                .andExpect(content().string(retorno));
+
+
     }
 
     @Test
-    void deletarTarefa() {
+    void deletarTarefa() throws Exception {
+        String nome = "Trabalhar";
+        doNothing().when(servico).deletar(nome);
+
+        mockMvc.perform(delete("/tarefas/nome"))
+                .andExpect(status().isOk());
+
     }
+
+
 }
